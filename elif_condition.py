17b3d74a1614d425baa521465e9436a4a6608e06
Alfred Turkson <111619866+Codeghod@users.noreply.github.com>
2022-09-17 16:56:57 -0700

    Add files via upload

---
+++ elif_condition.py
@@ -0,0 +1,24 @@
+# the elif comes in when there are multiple options in a case or scenario.
+
+a = 12
+
+if a < 20:
+    print("the number available is less than 20")
+    
+elif a == 20:
+    print("the number available is equal to 20")
+    
+else:
+    print("number is greater than 20")
+    
+b = 10
+
+if b < 10:
+    print("the value is the lower")
+    
+elif b == 10:
+    print("the number available is equal to 10")
+    
+else:
+    print("number is greater than 20")
+    
